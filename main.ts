12ca3bde660497b511661545959161a571904e3a
v7idea <louisch62@gmail.com>
2023-11-28 14:28:48 +0800

    增加超音波感測器進行閃躲避障

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+function RunCycle () {
+    maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
+    basic.pause(10)
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 255)
+    basic.pause(1000)
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 255)
+    basic.pause(500)
+    maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 255)
+    basic.pause(1000)
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 255)
+    basic.pause(1000)
+    maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 255)
+    basic.pause(1000)
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 255)
+    basic.pause(450)
+    maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
+    maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 255)
+    basic.pause(1000)
+}
 let RightSpeed = 0
 let LeftSpeed = 0
 maqueenPlusV2.I2CInit()
@@ -15,39 +36,50 @@ let SensorR1 = 0
 let SensorR2 = 0
 let IfChangeSpeed = 1
 let NormalSpeed = 100
+let frontDistance = 0
+let SlowSpeed1 = 75
+let SlowSpeed2 = 85
 basic.forever(function () {
     SensorL1 = 1 - maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorL1)
     SensorM = 1 - maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorM)
     SensorL2 = 1 - maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorL2)
     SensorR1 = 1 - maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorR1)
     SensorR2 = 1 - maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorR2)
+    frontDistance = maqueenPlusV2.readUltrasonic(DigitalPin.P13, DigitalPin.P14)
+    if (frontDistance < 1) {
+        frontDistance = 0
+    }
     if (SensorL2 == 1) {
         IfChangeSpeed = 1
         LeftSpeed = 255
-        RightSpeed = NormalSpeed - 60
+        RightSpeed = NormalSpeed - SlowSpeed1
     } else if (SensorR1 == 1) {
         IfChangeSpeed = 1
         RightSpeed = 255
-        LeftSpeed = NormalSpeed - 80
+        LeftSpeed = NormalSpeed - SlowSpeed2
     } else if (SensorR2 == 1) {
         IfChangeSpeed = 1
         RightSpeed = 255
-        LeftSpeed = NormalSpeed - 60
+        LeftSpeed = NormalSpeed - SlowSpeed1
     } else if (SensorL1 == 1) {
         IfChangeSpeed = 1
         LeftSpeed = 255
-        RightSpeed = NormalSpeed - 80
+        RightSpeed = NormalSpeed - SlowSpeed2
     } else if (LeftSpeed != NormalSpeed || RightSpeed != NormalSpeed) {
         IfChangeSpeed = 1
         RightSpeed = NormalSpeed
         LeftSpeed = NormalSpeed
     } else {
-    	
+        IfChangeSpeed = 1
     }
-    if (IfChangeSpeed == 1) {
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, LeftSpeed)
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, RightSpeed)
-        IfChangeSpeed = 0
+    if (frontDistance > 0 && frontDistance < 10) {
+        RunCycle()
+    } else {
+        if (IfChangeSpeed == 1) {
+            maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, LeftSpeed)
+            maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, RightSpeed)
+            IfChangeSpeed = 0
+        }
     }
     basic.pause(10)
 })
